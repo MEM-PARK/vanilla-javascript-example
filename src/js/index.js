270bf5666a214b2dd6455b150bf2ea519b914e40
朴 宗碩 <park.jongseok.4560@members.co.jp>
2023-05-09 14:27:33 +0900

    20230509

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 let age = prompt("おいくつですか？");
 age = parseInt(age);
+
 if (isNaN(age)) {
-  console.log("age is not a Number");
-} else if (isNan(asdf)) {
+  console.log("数字ではありません。😅");
+} else if (age >= 30) {
+  console.log("30より大きい数字です。");
 } else {
+  console.log("30より小さい数字です。");
 }

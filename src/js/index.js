c6fefb5e89b0a71e2ef1929ea030f40d894deb1e
朴 宗碩 <park.jongseok.4560@members.co.jp>
2023-05-10 15:05:44 +0900

    20230510

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
-let classA = document.querySelector(".classA");
-classA.innerText = "Hello";
+const classA = document.querySelector(".classA");
+
+// 1. functionはなに？
+// 2. constとfunctoinが何が違うの
+
+function handleClick() {
+  console.log("クリックされました！！");
+}
+
+classA.addEventListener("click", handleClick);

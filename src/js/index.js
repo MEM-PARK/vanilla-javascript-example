ef88e2014a718b4a135a8e1615027164da11b95c
朴 宗碩 <park.jongseok.4560@members.co.jp>
2023-06-21 14:01:49 +0900

    Reset

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-const loginForm = document.querySelector("#login-form");
-
-const onLoginBtnClick = (event) => {
-  event.preventDefault();
-  console.log(event.target);
-};
-
-loginForm.addEventListener("submit", onLoginBtnClick);

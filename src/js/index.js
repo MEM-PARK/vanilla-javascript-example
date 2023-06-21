7cb4f7101eccad22af8b7ba7212a9c951c7b50aa
朴 宗碩 <park.jongseok.4560@members.co.jp>
2023-06-21 14:18:51 +0900

    20230621

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,7 @@ const h1 = document.querySelector("h1");
 const button = document.querySelector("button");
 
 const handleClick = () => {
-  const fontColor = h1.style.color;
-  if (fontColor === "teal") {
-    h1.style.color = "orange";
-  } else {
-    h1.style.color = "teal";
-  }
+  h1.classList.toggle("teal");
 };
 
 button.addEventListener("click", handleClick);
